31120d6cadfa7c12140089f276fbfc48b6b3f4af
Elliott Alderman-Broom <elliott.aldermanbroom@gmail.com>
2022-12-08 22:47:55 +0000

    MUI Table

--- src/Components/DataTable.jsx
+++ src/Components/DataTable.jsx
@@ -1,4 +1,13 @@
 import React, {useState, useEffect} from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
 
 function DataTable() {
     const [repos, setRepos] = useState([])
@@ -17,14 +26,32 @@ function DataTable() {
 
 
     return (  
-        <div>
-            <ul>
-                {
-                repos.map (repo => <li key={repo.id}>{repo.name}</li>)
-                }  
-            </ul>
-        </div>
-    )
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                
+                <TableHead font-weight="bold">
+                <TableRow>
+                    <TableCell >Name</TableCell>
+                    <TableCell align="right">Stars</TableCell>
+                    <TableCell align="right">Forks</TableCell>
+                </TableRow>
+                </TableHead>
+
+                <TableBody>
+                {repos.map((repo) => (
+                    <TableRow
+                    key={repo.id}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                    <TableCell component="th" scope="row">{repo.full_name}</TableCell>
+                    <TableCell align="right">{repo.stargazers_count}</TableCell>
+                    <TableCell align="right">{repo.forks_count}</TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+            </TableContainer>
+  );
 }
 
 export default DataTable
