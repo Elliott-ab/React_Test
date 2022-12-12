a109c1983fdbd7315af924abe31a44d49aefbead
Elliott Alderman-Broom <elliott.aldermanbroom@gmail.com>
2022-12-12 21:48:06 +0000

    Pagination
    Adding pagination

--- src/Components/DataTable.jsx
+++ src/Components/DataTable.jsx
@@ -6,52 +6,81 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
+import TablePagination from '@mui/material/TablePagination';
 
 
 function DataTable() {
-    const [repos, setRepos] = useState([])
+  const [repos, setRepos] = useState([]);
 
     useEffect(() => {
-        fetch("https://api.github.com/search/repositories?q=react&per_page=10")
+      fetch("https://api.github.com/search/repositories?q=react&per_page=100")
         .then(res => res.json())
         .then(data => {
-            console.log(data)
             setRepos(data.items)
         })
         .catch(err => {
             console.log(err);
         })
-    }, [])
+    }, []);
 
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  
+    const handleChangePage = (event, newPage) => {
+      setPage(newPage);
+    };
+  
+    const handleChangeRowsPerPage = (event) => {
+      setRowsPerPage(parseInt(event.target.value, 10));
+      setPage(0);
+    };
+
+    const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, repos.length - page * rowsPerPage);
 
     return (  
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 
-                <TableHead font-weight="bold">
-                <TableRow>
-                    <TableCell >Name</TableCell>
-                    <TableCell align="right">Stars</TableCell>
-                    <TableCell align="right">Forks</TableCell>
+          <TableHead>
+          <TableRow>
+            <TableCell >Name</TableCell>
+            <TableCell align="right">Stars</TableCell>
+            <TableCell align="right">Forks</TableCell>
                 </TableRow>
                 </TableHead>
 
                 <TableBody>
-                {repos.map((repo) => (
-                    <TableRow
-                    key={repo.id}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
+                {/* {repos.map((repo) => ( */}
+                {repos
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((repo, index) => (
+                    <TableRow key={repo.id}>
                     <TableCell component="th" scope="row">{repo.full_name}</TableCell>
                     <TableCell align="right">{repo.stargazers_count}</TableCell>
                     <TableCell align="right">{repo.forks_count}</TableCell>
                     </TableRow>
                 ))}
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={3} />
+          </TableRow>
+        )}
                 </TableBody>
             </Table>
+            <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={100}
+            page={page}
+            onPageChange={handleChangePage}
+            rowsPerPage={rowsPerPage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
             </TableContainer>
+            
   );
 }
 
-export default DataTable
+
+export default DataTable;
